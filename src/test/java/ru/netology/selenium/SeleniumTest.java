a1aab5dd039e7a6aa30a423f392720d0338a3529
tpecherkina <74115313+tpecherkina@users.noreply.github.com>
2021-06-07 22:05:46 -0400

    Update SeleniumTest.java

--- src/test/java/ru/netology/selenium/SeleniumTest.java
+++ src/test/java/ru/netology/selenium/SeleniumTest.java
@@ -1,6 +1,7 @@
 package ru.netology.selenium;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
