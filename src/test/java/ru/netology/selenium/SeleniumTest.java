ff5711c8119bbf826522189c4601d0e1913ed395
tpecherkina <74115313+tpecherkina@users.noreply.github.com>
2021-06-20 20:09:55 -0400

    Update SeleniumTest.java

--- src/test/java/ru/netology/selenium/SeleniumTest.java
+++ src/test/java/ru/netology/selenium/SeleniumTest.java
@@ -70,9 +70,9 @@ public class SeleniumTest {
 
     @Test
     void shouldNotPromptToTheNextPageAgreementUnchecked() {
-       driver.findElement(cssSelector("[type='text']")).sendKeys("Таня");
-        driver.findElement(cssSelector("[type='tel']")).sendKeys("+72022571580");
-        driver.findElement(className("button__text")).click();
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Таня");
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+79195631111");
+           driver.findElement(cssSelector("span[class=button__text]")).click();
         assertTrue(!driver.findElements(cssSelector(".input_invalid ")).isEmpty(), "Не нажат флажок согласия на обработку персональных данных");
     }
 }
